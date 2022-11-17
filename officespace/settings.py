e309eedb27504202b1ec570d6600fa432e37a182
teresabowe <teresa.bowe@gmail.com>
2022-11-17 11:20:40 +0000

    build: update allowed hosts for Heroku and localhost

--- officespace/settings.py
+++ officespace/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['deskbooking.herokuapp.com', 'localhost']
 
 # Application definition
 
