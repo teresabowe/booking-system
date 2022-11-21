9a2bd795f6b1960909ee12c3994302890a154a2a
teresabowe <teresa.bowe@gmail.com>
2022-11-21 09:56:22 +0000

    build: prepare the admin interface

--- deskbooking/admin.py
+++ deskbooking/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Desk
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Desk)
+class DeskAdmin(admin.ModelAdmin):
+    list_display = ('desk', 'floor', 'description', 'created_on')
+    list_filter = ('floor', 'created_on')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('desk', 'desk_user', 'desk_booking_date', 'created_on')
+    list_filter = ('desk_user', 'created_on')
