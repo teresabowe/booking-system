30fa2151b0b913a52d9401a8a41b22dea65882ea
teresabowe <teresa.bowe@gmail.com>
2022-11-25 16:03:50 +0000

    feat: add new page to view bookings

--- deskbooking/urls.py
+++ deskbooking/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.view_home, name='home'),
+    path("view_bookings/", views.BookingsList.as_view(), name="view_bookings"),
 ]
--- deskbooking/views.py
+++ deskbooking/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from .models import Booking
 
 
 def view_home(request):
@@ -6,3 +9,18 @@ def view_home(request):
     View Home Page
     """
     return render(request, 'index.html')
+
+
+class BookingsList(LoginRequiredMixin, generic.ListView):
+    """
+    View Bookings
+    """
+    login_url = '../accounts/login/'
+    redirect_field_name = 'account_login'
+    model = Booking
+    queryset = Booking.objects.all()
+    template_name = "view_bookings.html"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(desk_user=self.request.user).\
+         order_by('desk_booking_date')
